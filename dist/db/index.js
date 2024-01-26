64dcea0289a06fd642e109e6d97fed4f24264b2b
Juan Salvador Magán Valero <jmaganvalero@gmail.com>
2024-01-26 12:43:39 +0100

    refactor: dist folder

--- dist/db/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.client = void 0;
-const prisma_client_1 = require("prisma/prisma-client");
-exports.client = new prisma_client_1.PrismaClient();
-//# sourceMappingURL=index.js.map
